f23e0f3330fd0b0ba201dcbd660960bdb0fe52a6
Tristan Kay <tridkay90@gmail.com>
2019-05-30 09:42:43 +1000

    trial'

--- shityaknickers.js
+++ shityaknickers.js
@@ -23,10 +23,10 @@ const displayRandomMovie = () => {
   let movieTitle = document.createElement("p");
 
   if(randHorrors.length === 0) {
-    movieTitle.innerText = movie
-    container.appendChild(movieTitle)
-    randHorrors.push(movieTitle)
-    
+    // movieTitle.innerText = movie
+    // container.appendChild(movieTitle)
+    // randHorrors.push(movieTitle)
+    alert("virgin");
   } else {
     document.querySelector("p").remove()
     randHorrors.pop()
